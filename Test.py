fed78d3fd6c2702fdf91a630479864b7c7b05122
Leduc <polchop@live.fr>
2017-04-04 13:52:55 +0200

    Echantillonnage 50%
    TODO:
    AjoutSpeckel
    Interpolation

--- Test.py
+++ Test.py
@@ -1,44 +1,34 @@
 import numpy as np
+import math
 from PIL import Image
 
 
-def noisy(noise_typ, image):
-    if noise_typ == "gauss":
-        row, col, ch = image.shape
-        mean = 0
-        var = 0.1
-        sigma = var ** 0.5
-        gauss = np.random.normal(mean, sigma, (row, col, ch))
-        gauss = gauss.reshape(row, col, ch)
-        noisy = image + gauss
-        return noisy
-    elif noise_typ == "s&p":
-        row, col, ch = image.shape
-        s_vs_p = 0.5
-        amount = 0.004
-        out = np.copy(image)
-        # Salt mode
-        num_salt = np.ceil(amount * image.size * s_vs_p)
-        coords = [np.random.randint(0, i - 1, int(num_salt))
-                  for i in image.shape]
-        out[coords] = 1
-
-        # Pepper mode
-        num_pepper = np.ceil(amount * image.size * (1. - s_vs_p))
-        coords = [np.random.randint(0, i - 1, int(num_pepper))
-                  for i in image.shape]
-        out[coords] = 0
-        return out
-    elif noise_typ == "poisson":
-        vals = len(np.unique(image))
-        vals = 2 ** np.ceil(np.log2(vals))
-        noisy = np.random.poisson(image * vals) / float(vals)
-        return noisy
-    elif noise_typ == "speckle":
-        row, col, ch = image.shape
-        gauss = np.random.randn(row, col, ch)
-        gauss = gauss.reshape(row, col, ch)
-        noisy = image + image * gauss
-        return noisy
 
 
+def echantillonage(image, beamNumber, pxPerBeam, angle, height, dmin, dmax):
+    longeur, largeur = image.size
+    image = list(image.getdata())
+    image = [image[i * largeur:(i + 1) * largeur] for i in range(longeur)]
+    #Base sortie
+    imageSortie = np.zeros((longeur,largeur))
+    i = ((3 * math.pi) - angle)/2
+    j = dmin
+    while i <= ((3 * math.pi) + angle)/2:
+        while j <= dmax:
+            x = j*math.cos(i) + largeur/2
+            y = -j*math.sin(i) + height
+            x = int(math.fabs(x))
+            y = int(y)
+            print(x,y)
+            imageSortie[x][y] = image[x][y]
+            j += pxPerBeam
+        i += beamNumber
+    return imageSortie
+
+
+img = Image.open("images/fg1.bmp").convert("L")
+l,L = img.size
+print(l,L)
+img2 = echantillonage(img, 100, 2, 3, 10, 15, 350)
+img3 = Image.fromarray(img2)
+img3.show()
