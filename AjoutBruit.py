8f3cf160ad7ff0709749250e23d94bfb152ed0e0
Leduc <polchop@live.fr>
2017-04-07 11:58:46 +0200

    Echantillonnage 100% AjoutSpeclel 90% Interpolation 90% TODO Code Générique Gérer problème ratio pixel

--- AjoutBruit.py
+++ AjoutBruit.py
@@ -1,8 +1,11 @@
 import numpy as np
 import math
 from PIL import Image
+from scipy import interpolate
 from random import uniform
-
+from PIL import ImageOps
+import cv2
+from multiprocessing import Pool
 
 def echantillonageRadial(image, beamNumber, pxPerBeam, angle, height, dmin, dmax):
     longeur, largeur = image.size
@@ -27,10 +30,10 @@ def echantillonageRadial(image, beamNumber, pxPerBeam, angle, height, dmin, dmax
 
 def echantillonageRect(npimage, longeur, largeur, nbPointAbscisse, nbpointOrdonnee):
     # Image base sortie :
-    imageSortie = np.ones((longeur, largeur)) -2
-    y = 1
+    imageSortie = np.ones((longeur, largeur))-2
+    y = 0
     while y < largeur:
-        x=1
+        x=0
         while x < longeur:
             imageSortie[int(x)][int(y)] = npimage[int(x)][int(y)]
             x += largeur / nbPointAbscisse
@@ -38,17 +41,30 @@ def echantillonageRect(npimage, longeur, largeur, nbPointAbscisse, nbpointOrdonn
     return imageSortie
 
 
+def echantillonageRect2(npimage, longeur, largeur, nbPointAbscisse, nbpointOrdonnee):
+    y = 0
+    while y < largeur/2:
+        print(y)
+        npimage = np.delete(npimage,(y),axis=1)
+        y += 1
+    x = 0
+    while x < longeur/2:
+        print(x)
+        npimage = np.delete(npimage,(x),axis=0)
+        x += 1
+    return npimage
+
 
 def AjoutSpeckel( img, borneInf, borneSup, ecartTypeGauss):
-    print(img)
     longeur,largeur = img.shape
-    imgRetour = np.zeros((longeur, largeur))
-    y = 0
-    while y < largeur-1:
-        x=0
-        while x < longeur-1:
+    imgRetour = np.ones((longeur, largeur)) * 255
+    y = -1
+    while y < largeur -1:
+        x = -1
+        y += 1
+        while x < longeur-1 :
+            x += 1
             if img[x,y] != -1:
-                print(x,y)
                 px = np.sqrt(img[x,y]) + 0j
                 u = uniform(borneInf,borneSup)
                 i = 1
@@ -57,36 +73,56 @@ def AjoutSpeckel( img, borneInf, borneSup, ecartTypeGauss):
                     px += g[0,0]
                     px += np.complex(0,g[0,1])
                     i += 1
-                x += 1
                 imgRetour[x,y] = (np.square(px.real) + np.square(px.imag))
-            else:
-                x+= 1
-                imgRetour[x,y] = 255
-        y += 1
     return imgRetour
 
+
+def interpolation(img):
+    img = np.zeros(img.shape) + img
+    l,L = img.shape
+    x = np.arange(0,l,1)
+    y = np.arange(0, L, 1)
+    img = interpolate.interp2d(x,y,img, kind='cubic')
+    return img
+
+def construireImageInterpelee(function,l,L):
+
+    x = np.arange(0,l,1);
+    y = np.arange(0,L,1)
+    arr = np.zeros((l,L))
+    for yi in y:
+        for xi in x:
+         arr[xi][yi] = function(xi/2,yi/2)
+    print(arr)
+    img = Image.fromarray(arr)
+    img = ImageOps.mirror(img)
+    img.rotate(90).show()
+    return img
 
 
 def AjoutBruit(image):
     # conversion de l'image en array numpy
-    longeur, largeur = image.size
-    image = list(image.getdata())
-    image = [image[i * largeur:(i + 1) * largeur] for i in range(longeur)]
-    img2 = echantillonageRect(image, longeur, largeur, 190, 190)
-    img3 = AjoutSpeckel(img2, 2, 10, 2)
-    img4 = Image.fromarray(img3)
-    img4.show()
-
-
-def interpolation(img):
-
-    return 0
+    l,L = image.shape
+    img = echantillonageRect2(image, l ,L ,l/2,L/2)
+    print(img.shape)
+    img4 = AjoutSpeckel(img, 2, 10, 2)
+    # Image.fromarray(img4).show()
+    img5 = interpolation(img4)
+    img5 = construireImageInterpelee(img5,l,L)
 
 # MAIN
-img = Image.open("images/vador.bmp").convert("L")
+img = cv2.imread("images/leia.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 AjoutBruit(img)
 
-
+#
+# if __name__ == '__main__':
+#     pool = Pool(processes=4)
+#     possibleFactors = range(1,int(np.floor(np.sqrt(numToFactor)))+1)
+#     print 'Checking ', possibleFactors
+#     result = pool.map(isFactor, possibleFactors)
+#     cleaned = [x for x in result if not x is None]
+#     print 'Factors are', cleaned
 # img = Image.open("images/fg1.bmp").convert("L")
 # l, L = img.size
 # print(l, L)
@@ -95,3 +131,5 @@ AjoutBruit(img)
 
 # img3.show()
 #
+
+
