164437bb75fc7896556de2f39be349009500d241
Leduc <polchop@live.fr>
2017-04-13 13:43:14 +0200

    Analyse d'image echographique d'un dossier ok Vectorialisation Ok Echantillonnage 100% AjoutSpeclel 100% Interpolation 100% Analyse de l'image 100% Multithreading 80% ( TODO : connaitre nombre de coeur du processeur local) TODO Générer loi Normal et GenNormal à donner au fonctions d'ajout du bruit de speckle (parallélisation)

--- AjoutBruit.py
+++ AjoutBruit.py
@@ -10,42 +10,9 @@ import threading
 import os
 # import pyopencl as cl
 
-def echantillonageRadial(image, beamNumber, pxPerBeam, angle, height, dmin, dmax):
-    longeur, largeur = image.size
-    image = list(image.getdata())
-    image = [image[i * largeur:(i + 1) * largeur] for i in range(longeur)]
-    # Base sortie
-    imageSortie = np.zeros((longeur, largeur))
-    i = (3 * math.pi - angle) / 2
-    while i < (3 * math.pi + angle) / 2:
-        j = dmin
-        while j < dmax:
-            x = j * math.cos(i) + largeur / 2
-            y = -j * math.sin(i) + height
-            x = int(x)
-            y = int(y)
-            imageSortie[x][y] = image[x][y]
-
-            j += (((3 * math.pi + angle) / 2) - ((3 * math.pi - angle) / 2)) / (pxPerBeam - 1)
-        i += (dmax - dmin) / (beamNumber - 1)
-    return imageSortie
-
-
-def echantillonageRect(npimage, longeur, largeur, nbPointAbscisse, nbpointOrdonnee):
-    # Image base sortie :
-    imageSortie = np.ones((longeur, largeur))-2
-    y = 0
-    while y < largeur:
-        x=0
-        while x < longeur:
-            imageSortie[int(x)][int(y)] = npimage[int(x)][int(y)]
-            x += largeur
-        y += longeur/ nbpointOrdonnee
-    return imageSortie
-
-
-def echantillonageRect2(npimage,nbPoint):
+def echantillonageRect(npimage,nbPoint):
     """
+    Echantillone l'image selon le ration du nombre de point que l'on veut garder
     
     :param npimage: Image à echantilloner en format numpy 
     :param nbPoint: Ratio des points que l'on veut garder : 1 sur nbpoint
@@ -69,31 +36,32 @@ def echantillonageRect2(npimage,nbPoint):
     return npimage
 
 
-def AjoutSpeckel( img, borneInf, borneSup, ecartTypeGauss, u):
+def ajoutSpeckelGenNorm(img, alpha, gamma):
     longeur,largeur = img.shape
-    #matrices de vecteurs gaussiens
-    beta = 8
-    alpha = 1.98
-    # matrixGauss = stats.gennorm.rvs(beta,scale=alpha,loc=0,size=longeur*longeur*u).reshape(longeur, largeur, u)
-    # matrixGauss2 =  stats.gennorm.rvs(beta,scale=alpha,loc=0,size=longeur*longeur*u).reshape(longeur, largeur, u)
-    # matrixGauss = (stats.levy_stable_gen.rvs(alpha, 0,size = longeur*largeur*u,scale=gama).reshape(longeur, largeur, u))
-    # matrixGauss2 = (stats.levy_stable_gen.rvs(alpha,0,size = longeur*largeur*u,scale=gama).reshape(longeur, largeur, u))
-    matrixGauss = np.random.randn(longeur * largeur, u).reshape(longeur, largeur, u)
-    matrixGauss2 = np.random.randn(longeur * largeur, u).reshape(longeur, largeur, u)
+    #matrices de vecteurs généralisation de loi gaussienne :
+    matrixGauss = stats.gennorm.rvs(gamma,scale=alpha,loc=0,size=longeur*longeur).reshape(longeur, largeur)
+    matrixGauss2 =  stats.gennorm.rvs(gamma,scale=alpha,loc=0,size=longeur*longeur).reshape(longeur, largeur)
     imgRetour = np.sqrt(img + 0j)
-    i = 0
-    while i < u-1:
-        imgRetour += (matrixGauss[:,:,i]) + (matrixGauss2[:,:,i]*1j)
-        i += 1
+    imgRetour += (matrixGauss[:,:]) + (matrixGauss2[:,:]*1j)
     img = np.square(imgRetour.real) + np.square(imgRetour.imag)
     return img
 
 
+def ajoutSpeckelGauss(img, ecartType):
+    longeur,largeur = img.shape
+    #matrices de vecteurs loi normal :
+    matrixGauss =  ecartType + np.random.randn(longeur * largeur).reshape(longeur, largeur)
+    matrixGauss2 = ecartType + np.random.randn(longeur * largeur).reshape(longeur, largeur)
+    imgRetour = np.sqrt(img + 0j)
+    imgRetour += (matrixGauss[:,:]) + (matrixGauss2[:,:]*1j)
+    img = np.square(imgRetour.real) + np.square(imgRetour.imag)
+    return img
+
 def interpolation(img):
     """
     Interpolation bicubique
-    :param img: Image à interpoler sous forme de tableau numpy
-    :return: Fonction d'interpoaltion de l'image 
+    :param img: Image à interpoler, sous forme de tableau numpy
+    :return: Une fonction d'interpoaltion de l'image 
     """
     img = np.zeros(img.shape) + img
     l,L = img.shape
@@ -118,6 +86,34 @@ def construireImageInterpelee(function,l,L,nbPoint):
     return img
 
 
+def anayseImageCapteur(path):
+    """
+    Analyse les paramètre du capteurs 
+    :param path: 
+    :return: Une liste de la variation des intensités des images, de Alpha, et de Gamma
+    """
+    listPx = [0]
+    listeImage = os.listdir(path)
+    listVar = []
+    listGama = []
+    listAlpha = []
+    for image in listeImage:
+        pathIm= "%s\%s"%(path,image)
+        print(pathIm)
+        img = cv2.imread(pathIm,1)
+        #Supprime les valeurs 0 et 255
+        mask = np.logical_and(img != 0,img != 255)
+        img = img[mask]
+        var = np.var(img)
+        esp = np.mean(img)
+        alpha = np.sqrt(np.square(math.pi) / (6 * var))
+        gama = math.exp(alpha * (esp - math.log(2, 10) - 0.5772156 * ((1 / alpha) - 1)))
+        listVar.append(esp)
+        listAlpha.append(alpha)
+        listGama.append(gama)
+    print(listVar,listAlpha,listGama)
+
+
 def AjoutBruit(image):
     """
     Fonction principale appelant les méthodes permettant d'ajouter un bruit de peckel à image de type optique
@@ -128,8 +124,8 @@ def AjoutBruit(image):
     # conversion de l'image en array numpy
     nbPoint = 2
     l,L = image.shape
-    img = echantillonageRect2(image,nbPoint)
-    img4 = AjoutSpeckel(img, 1,1 , 0.2,3)
+    img = echantillonageRect(image,nbPoint)
+    img4 = ajoutSpeckelGenNorm(img,1.8,8)
     img5 = interpolation(img4)
     img5 = construireImageInterpelee(img5,l,L,nbPoint)
     return img5
@@ -168,59 +164,7 @@ def multipleImage(img,p):
          save("imgT1It%sP%s"%(it,p),"gif")
         it += 1
 
-def trouveAlphaGama():
-    img = cv2.imread("images/01_d_3.bmp")
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    l,L = img.shape
-    y = -1
-    somme = 0
-    sommeC = 0
-    cpt = 0
-    a = [0]
-    while y < L-1:
-        y +=1
-        x = -1
-        while x < l-1:
-            print(x,y)
-            x += 1
-            if ( img[x][y] != 0):
-                a.append(img[x][y])
 
-    print(a)
-    esp = np.mean(a,dtype=np.float64)
-    var = np.var(a,dtype=np.float64)
-    alpha = np.sqrt(np.square(math.pi)/(6*var))
-    gama = math.exp( alpha * (esp - math.log(2,10) - 0.57721566490153286*((1/alpha)-1) ))
-    print(esp,var,alpha,gama)
-
-
-
-def anayseImageCapteur(path):
-    listPx = [0]
-    listeImage = os.listdir(path)
-    for image in listeImage:
-        p= "%s\%s"%(path,image)
-        y = -1
-        try :
-            imgTmp = cv2.imread(imgTmp)
-            l,L = imgTmp.shape
-            while y < L-1:
-                y +=1
-                x = -1
-                while x < l-1:
-                    print(x,y)
-                    x += 1
-                    if ( imgTmp[x][y] != 0):
-                        listPx.append(imgTmp[x][y])
-        except: print("Impossible d'analyser l'image %s"%(p))
-
-
-        esp = np.mean(listPx, dtype=np.float64)
-        var = np.var(listPx, dtype=np.float64)
-        alpha = np.sqrt(np.square(math.pi) / (6 * var))
-        gama = math.exp(alpha * (
-            esp - math.log(2, 10) - 0.5772156 * ((1 / alpha) - 1)))
-        print (esp,var,alpha,gama)
 
 # MAIN
 anayseImageCapteur("C:/Users\polch_000\Desktop\ImagesEchographiques")
