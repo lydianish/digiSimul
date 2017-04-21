1a2de3558775ed17bedadc7fd15b1af17f34dc5d
Leduc <polchop@live.fr>
2017-04-21 22:04:10 +0200

    VersionFinal 1

--- Digisimul.py
+++ Digisimul.py
@@ -7,28 +7,24 @@ import os
 import numpy as np
 import cv2
 from PIL import Image
+import multiprocessing
+import threading
 
-def modeliserImagesAutoPrecision(pathCapteur,pathBdd, nombre, nbPoint, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
-# Analyse du capteur :
-    var,alpha,gama = AjoutBruit.anayseImageCapteur(pathCapteur)
-#Determination des fonctions de probabilité type kernel :
-    fctAlpha = stats.gaussian_kde(alpha)
-    fctGama = stats.gaussian_kde(gama)
-
+def modeliserImagesAutoPrecision(pathBdd, nombre, nbPoint, drfAlpha, drfGama, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
 #Tirage d'une image de la bdd
     listeImage = os.listdir(pathBdd)
     nbImageBdd = len(listeImage)
     it = 1
     while it < nombre:
     #Selection d'une valeur de la loi kernel:
-        alpha = fctAlpha.resample(1)
-        gama = fctGama.resample(1)
+        alpha = drfAlpha.resample(1)
+        gama = drfGama.resample(1)
     #tirage de l'image en Bdd
         tirage = np.random.uniform(0,nbImageBdd -1)
         imgTire = listeImage[int(tirage)]
     # Découpage de l'image
 
-
+        var = None
     #Ajout du bruit :
         pathIm = "%s\%s" % (pathBdd, imgTire)
         img = cv2.imread(pathIm)
@@ -38,12 +34,14 @@ def modeliserImagesAutoPrecision(pathCapteur,pathBdd, nombre, nbPoint, largeur, 
         it+=1
 
 
-def modeliserImagesAutoRapide(pathBdd, nombre, nbPoint, var, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
+def modeliserImagesAutoRapide(pathBdd, nombre, nbPoint, drfVar, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
 #Tirage d'une image de la bdd
     listeImage = os.listdir(pathBdd)
     nbImageBdd = len(listeImage)
     it = 1
     while it < nombre:
+    #Selection d'une valeur de la loi kernel:
+        var = drfVar.resample(1)
     #tirage de l'image en Bdd
         tirage = np.random.uniform(0,nbImageBdd -1)
         imgTire = listeImage[int(tirage)]
@@ -104,6 +102,39 @@ def modeliserImagesManuelPrecision(pathBdd, nombre, nbPoint, alpha, gama, largeu
         img = AjoutBruit.AjoutBruit(img, nbPoint,"gen",var,alpha,gama)
         Image.fromarray(img).show()
         it+=1
+
+
+def modeliserImagesAutoPrecisionMultithread(pathCapteur, pathBdd, nombre, nbPoint, largeur, longueur, ecartTypeX, ecartTypeY, ecartTypeAngle):
+# Analyse du capteur :
+    var, alpha, gama = AjoutBruit.anayseImageCapteur(pathCapteur)
+# Determination des fonctions de probabilité type kernel :
+    drfAlpha = stats.gaussian_kde(alpha)
+    drfGama = stats.gaussian_kde(gama)
+    nbCore = multiprocessing.cpu_count()-1
+    nombre2 = nombre/nbCore #
+    it = 1
+    p = {}
+    while it < nbCore:
+        p[it]= threading.Thread(target=modeliserImagesAutoPrecision,args=(pathBdd, nombre2, nbPoint, drfAlpha, drfGama, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle) )
+        p[it].start()
+        it += 1
+
+
+def modeliserImagesAutoRapideMultithread(pathCapteur, pathBdd, nombre, nbPoint, largeur, longueur, ecartTypeX, ecartTypeY, ecartTypeAngle):
+# Analyse du capteur :
+    var, alpha, gama = AjoutBruit.anayseImageCapteur(pathCapteur)
+# Determination des fonctions de probabilité type kernel :
+    drfVar = stats.gaussian_kde(var)
+    nbCore = multiprocessing.cpu_count()-1
+    nombre2 = nombre/nbCore #
+    it = 1
+    p = {}
+    while it < nbCore:
+        p[it]= threading.Thread(target=modeliserImagesAutoRapide, args=(pathBdd, nombre2, nbPoint, drfVar, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle) )
+        p[it].start()
+        it += 1
+
+
 
 #main test
 modeliserImagesAutoPrecision("C:/Users\polch_000\Desktop\ImagesEchographiques","C:/Users\polch_000\Desktop\imagesBdd",2,4,5,5,5,5,5)
