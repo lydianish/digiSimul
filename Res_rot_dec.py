1694876061972f033a318157985739dba9eefd1e
noemie6 <nonoemie6@hotmail.fr>
2017-04-25 15:33:43 +0200

    Update Res_rot_dec.py

--- Res_rot_dec.py
+++ Res_rot_dec.py
@@ -1,5 +1,5 @@
-
 import cv2
+import numpy as np
 from PIL.Image import *
 import re
 
@@ -8,77 +8,54 @@ from Rotation import rotation
 from Decoupage import decoupe
 
 
-def DonneNom(chemin):
+def ech_rot_dec(image, ro, rc, angle, x, y, taille_capteur_long, taille_capteur_hauteur):
     """
-    
     :param chemin: chemin où se trouve l'image
-    :return: le nom de l'image quand on lui donne un chemin
+    :param ro: resolutionOriginale (c'est la resolution de l'image originale dont le chemin est donne en parametre)
+    :param rc: resolutionCapteur (c'est la resolution finale)
+    :param angle: l'angle de rotation
+    :param x: coordonnées de l'abscisse du pixel en haut à gauche où l'on veut découper
+    :param y: coordonnées de l'ordonné du pixel en haut à gauche où l'on veut découper
+    :param taille_capteur_long: taille du capteur en longueur
+    :param taille_capteur_hauteur: taille du capteur en hauteur
+    :return: l'image ayant subi un re-échantillonage, une rotation et un découpage
     """
-    l = chemin.split("/")
-    r = l[len(l) - 1]
-    return r
+    # taille de l'image
 
+    im = open(image)
+    tl,th=im.size
 
-class Res_rot_dec :
+    # echantillonnage & sauvegarde de l'image
+    (im3) = re_echantillonnage(ro, rc, im)
+    im3.save("resolution.png", "PNG")
+    im2 = cv2.imread('resolution.png', -2)
 
-    # Methode qui renvoie l'image ayant subi un re-échantillonage, une rotation et un découpage
-    # Donées :
-    #   - chemin : chemin où se trouve l'image
-    #   - ro:
-    #   - rc:
-    #   - angle :
-    #   - (x,y)  :
-    #   - taille du capteur: -taille_capteur_longueur
-    #                        - taille_capteur_hauteur
+    # rotation et sauvegarde
+    rot = rotation(im2, angle)
 
-    def ech_rot_dec(chemin,ro,rc,angle, x, y, taille_capteur_long, taille_capteur_hauteur):
-        """
-        :param chemin: chemin où se trouve l'image
-        :param ro: resolutionOriginale (c'est la resolution de l'image originale dont le chemin est donne en parametre)
-        :param rc: resolutionCapteur (c'est la resolution finale)
-        :param angle: l'angle de rotation
-        :param x: coordonnées de l'abscisse du pixel en haut à gauche où l'on veut découper
-        :param y: coordonnées de l'ordonné du pixel en haut à gauche où l'on veut découper
-        :param taille_capteur_long: taille du capteur en longueur
-        :param taille_capteur_hauteur: taille du capteur en hauteur
-        :return: l'image ayant subi un re-échantillonage, une rotation et un découpage
-        """
-        #recupere le nom de l'image
-        nom_image = DonneNom(chemin)
+    # Nouvelles valeurs de x, y , taille_capteur
+    hautmin, longmin = rot.shape[:2]
+    xnouv = x * longmin / tl
+    ynouv = y * hautmin / th
+    tcl = taille_capteur_hauteur * hautmin / th
+    tch = taille_capteur_long * longmin / tl
 
-        #echantillonnage & sauvegarde de l'image
-        (im3) = re_echantillonnage(ro, rc, chemin)
-        im3.save("resolution.png","PNG")
-        im2 = cv2.imread('resolution.png', -2)
+    #Passer d'une image à un tableau numpy
+    tab = np.array(rot)
+    h, l = tab.shape[:2]
+    # découpage
+    n = decoupe(tab, round(xnouv), round(ynouv), round(tcl), round(tch),l,h)
+    
+    # format d'enregistrement: nom_angle_x_y
+    # m = re.search('[^.]*', image)
+    # prefixe = m.group(0)
+    # a = "%s_angle%s" % (prefixe, angle)
+    # b = "%s_x%s" % (a, x)
+    # nom = "%s_y%s" % (b, y)
+    # ext = ".png"
+    # nom_complet = "%s%s" % (nom, ext)
+    # n.save(nom_complet, "PNG")
+    os.remove('resolution.png')
 
-        #rotation et sauvegarde
-        rot = rotation(im2, angle)
-        cv2.imwrite('rotation.jpg', rot)
-        ima = open('rotation.jpg')
-
-        #Nouvelles valeurs de x, y , taille_capteur
-        hautmin, longmin = ima.size
-        lim = open(nom_image)
-        hautmax, longmax = lim.size
-        xnouv = x * longmin / longmax
-        ynouv = y * hautmin / hautmax
-        tcl = taille_capteur_hauteur * hautmin / hautmax
-        tch = taille_capteur_long * longmin / longmax
-
-        #découpage
-        n = decoupe(ima, xnouv, ynouv, tcl, tch)
-
-        # format d'enregistrement: nom_angle_x_y
-        m = re.search('[^.]*', nom_image)
-        prefixe = m.group(0)
-        a = "%s_angle%s" % (prefixe, angle)
-        b = "%s_x%s" % (a, x)
-        nom = "%s_y%s" % (b, y)
-        ext = ".png"
-        nom_complet = "%s%s" % (nom, ext)
-        n.save(nom_complet, "PNG")
-        os.remove('resolution.png')
-        os.remove('rotation.jpg')
-
-#test de la méthode
-    # ech_rot_dec("/Users/Noemie/PycharmProjects/Projet/101_1.tif",500,200,0, 100, 200, 100, 100)
+# test de la méthode
+ech_rot_dec('03_d_1.bmp', 500, 200, 0, 120, 278, 200, 200)
