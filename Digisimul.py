4598ca8331a94c6e402cc78ca8a3b94deacadcdd
Leduc <polchop@live.fr>
2017-04-25 15:46:10 +0200

    Version 0.5

--- Digisimul.py
+++ Digisimul.py
@@ -1,140 +1,112 @@
 import AjoutBruit
 import Res_rot_dec
-import Positionnement
+import LoisPositionnement
 import Préliminaire
 from scipy import stats
+import Images
 import os
 import numpy as np
 import cv2
 from PIL import Image
 import multiprocessing
 import threading
+import Res_rot_dec
+import Resolution
 
-def modeliserImagesAutoPrecision(pathBdd, nombre, nbPoint, drfAlpha, drfGama, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
-#Tirage d'une image de la bdd
+nombreImagesGen = 0
+
+def modeliserImage(pathBdd, pathSave, nomImages, nombreImages, nbPoints,resolutionOriginal, resolutionCapteur, largeur, longueur, var, alpha, gama,minX,maxX,minY,maxY,minAngle,maxAngle, methode):
+    global nombreImagesGen
+    itImage = 0
+    numeroImage = 0
+    # pour chaque image
+    reste = nombreImages % len(nomImages)
+    while itImage < len(nomImages):
+        it = 0
+        #Ouverture de l'image
+        pathIm = "%s\%s" % (pathBdd, nomImages[numeroImage])
+        img = cv2.imread(pathIm,-1)
+    # Sous-résolution
+        # img  = Resolution.re_echantillonnage(resolutionOriginal,resolutionCapteur,img)
+
+        nombreImages = nombreImages // len(nomImages)
+        if (reste > 0):
+            nombreImages += 1
+            reste -= 1
+        while it <= nombreImages:
+        # Découpage et rotation de l'image
+            (x,y,theta) = LoisPositionnement.unePosition(minX,maxX,minY,maxY,minAngle,maxAngle)
+            # Res_rot_dec.Res_rot_dec.ech_rot_dec()
+
+        # Ajout du bruit:
+            img = AjoutBruit.AjoutBruit(img, nbPoints, methode, var, alpha, gama)
+            it += 1
+            # Image.fromarray(img).show()
+            nombreImagesGen += 1
+            Image.fromarray(img).save("%s/%s%s%s.gif" %(pathSave,nomImages[numeroImage],it,itImage))
+            #TODO enregistrer l'image
+
+        print(nombreImagesGen)
+        itImage += 1
+    return img
+
+
+def modeliserImagesMultithread(pathCapteur, pathBdd, pathSave, nombreImages, nbPoints, resolutionOriginal, resolutionCapteur, largeur, longueur, minX,maxX,minY,maxY,minAngle,maxAngle, analyse = False ,methodeLente = False, var = None, alpha = None, gama = None ,coeursLibres = 1 ):
+
+    if analyse == True:
+        # Analyse du capteur :
+        var, alpha, gama = AjoutBruit.anayseImageCapteur(pathCapteur)
+        if methodeLente == True:
+            # Determination des fonctions de probabilité type kernel :
+            drfVar = stats.gaussian_kde(var)
+        if methodeLente == False:
+            drfAlpha = stats.gaussian_kde(alpha)
+            drfGama = stats.gaussian_kde(gama)
+
+
+
     listeImage = os.listdir(pathBdd)
-    nbImageBdd = len(listeImage)
-    it = 1
-    while it < nombre:
-    #Selection d'une valeur de la loi kernel:
-        alpha = drfAlpha.resample(1)
-        gama = drfGama.resample(1)
-    #tirage de l'image en Bdd
-        tirage = np.random.uniform(0,nbImageBdd -1)
-        imgTire = listeImage[int(tirage)]
-    # Découpage de l'image
+    nbCore = multiprocessing.cpu_count() - coeursLibres
 
-        var = None
-    #Ajout du bruit :
-        pathIm = "%s\%s" % (pathBdd, imgTire)
-        img = cv2.imread(pathIm)
-        img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        img = AjoutBruit.AjoutBruit(img, nbPoint,"gen",var,alpha,gama)
-        Image.fromarray(img).show()
-        it+=1
+    listeImageCut = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    index = -1
+    #Repartition des images par coeur
+    print('nbCore: %s'%(nbCore))
+    while index < len(listeImage)-1:
+        index += 1
+        listeImageCut[index % nbCore].append(listeImage[index])
 
+     #Création et lancements des processus
+    it = 0
+    processus = {}
 
-def modeliserImagesAutoRapide(pathBdd, nombre, nbPoint, drfVar, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
-#Tirage d'une image de la bdd
-    listeImage = os.listdir(pathBdd)
-    nbImageBdd = len(listeImage)
-    it = 1
-    while it < nombre:
-    #Selection d'une valeur de la loi kernel:
-        var = drfVar.resample(1)
-    #tirage de l'image en Bdd
-        tirage = np.random.uniform(0,nbImageBdd -1)
-        imgTire = listeImage[int(tirage)]
-    # Découpage de l'image
-
-    #Ajout du bruit :
-        pathIm = "%s\%s" % (pathBdd, imgTire)
-        img = cv2.imread(pathIm)
-        img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        img = AjoutBruit.AjoutBruit(img, nbPoint,"norm",var,alpha=None,gama=None)
-        Image.fromarray(img).show()
-        it+=1
-
-
-def modeliserImagesManuelRapide(pathCapteur,pathBdd, nombre, nbPoint, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
-# Analyse du capteur :
-    var,alpha,gama = AjoutBruit.anayseImageCapteur(pathCapteur)
-#Determination des fonctions de probabilité type kernel :
-    fctVar = stats.gaussian_kde(var)
-#Tirage d'une image de la bdd
-    listeImage = os.listdir(pathBdd)
-    nbImageBdd = len(listeImage)
-    it = 1
-    while it < nombre:
-    #Selection d'une valeur de la loi kernel:
-        var = fctVar.resample(1)
-    #tirage de l'image en Bdd
-        tirage = np.random.uniform(0,nbImageBdd -1)
-        imgTire = listeImage[int(tirage)]
-    # Découpage de l'image
-
-    #Ajout du bruit :
-        pathIm = "%s\%s" % (pathBdd, imgTire)
-        img = cv2.imread(pathIm)
-        img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        img = AjoutBruit.AjoutBruit(img, nbPoint,"norm",var,alpha,gama)
-        Image.fromarray(img).show()
-        it+=1
-
-
-def modeliserImagesManuelPrecision(pathBdd, nombre, nbPoint, alpha, gama, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle):
-#Tirage d'une image de la bdd
-    listeImage = os.listdir(pathBdd)
-    nbImageBdd = len(listeImage)
-    var = None
-    it = 1
-    while it < nombre:
-    #tirage de l'image en Bdd
-        tirage = np.random.uniform(0,nbImageBdd -1)
-        imgTire = listeImage[int(tirage)]
-    # Découpage de l'image
-
-
-    #Ajout du bruit :
-        pathIm = "%s\%s" % (pathBdd, imgTire)
-        img = cv2.imread(pathIm)
-        img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        img = AjoutBruit.AjoutBruit(img, nbPoint,"gen",var,alpha,gama)
-        Image.fromarray(img).show()
-        it+=1
-
-
-def modeliserImagesAutoPrecisionMultithread(pathCapteur, pathBdd, nombre, nbPoint, largeur, longueur, ecartTypeX, ecartTypeY, ecartTypeAngle):
-# Analyse du capteur :
-    var, alpha, gama = AjoutBruit.anayseImageCapteur(pathCapteur)
-# Determination des fonctions de probabilité type kernel :
-    drfAlpha = stats.gaussian_kde(alpha)
-    drfGama = stats.gaussian_kde(gama)
-    nbCore = multiprocessing.cpu_count()-1
-    nombre2 = nombre/nbCore #
-    it = 1
-    p = {}
+    reste = nombreImages % nbCore
     while it < nbCore:
-        p[it]= threading.Thread(target=modeliserImagesAutoPrecision,args=(pathBdd, nombre2, nbPoint, drfAlpha, drfGama, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle) )
-        p[it].start()
+        nombreImagesCore = (nombreImages // nbCore)
+        if (reste > 0):
+            nombreImagesCore += 1
+            reste -= 1
+        print("nbImgCore %s"%nombreImagesCore)
+        processus[it]= threading.Thread(target=modeliserImage, args=(pathBdd, pathSave,listeImageCut[it], nombreImagesCore, nbPoints,resolutionOriginal, resolutionCapteur, largeur, longueur, var, alpha, gama,minX,maxX,minY,maxY,minAngle,maxAngle,"norm"))
+        processus[it].start()
         it += 1
-
-
-def modeliserImagesAutoRapideMultithread(pathCapteur, pathBdd, nombre, nbPoint, largeur, longueur, ecartTypeX, ecartTypeY, ecartTypeAngle):
-# Analyse du capteur :
-    var, alpha, gama = AjoutBruit.anayseImageCapteur(pathCapteur)
-# Determination des fonctions de probabilité type kernel :
-    drfVar = stats.gaussian_kde(var)
-    nbCore = multiprocessing.cpu_count()-1
-    nombre2 = nombre/nbCore #
-    it = 1
-    p = {}
-    while it < nbCore:
-        p[it]= threading.Thread(target=modeliserImagesAutoRapide, args=(pathBdd, nombre2, nbPoint, drfVar, largeur, longueur,ecartTypeX, ecartTypeY, ecartTypeAngle) )
-        p[it].start()
-        it += 1
-
-
 
 #main test
-modeliserImagesAutoPrecision("C:/Users\polch_000\Desktop\ImagesEchographiques","C:/Users\polch_000\Desktop\imagesBdd",2,4,5,5,5,5,5)
+
+pathCapteur =  "C:/Users\polch_000\Desktop\ImagesEchographiques"
+pathBdd = "C:/Users\polch_000\Desktop\imagesBdd"
+pathSave = "C:/Users\polch_000\Desktop\imagesRes"
+nombreImages = 120
+nbPoints = 6
+resolutionOriginal = 200
+resolutionCapteur = 300
+largeur = 100
+longueur = 100
+minX,maxX,minY,maxY,minAngle,maxAngle = 0,100,0,100,0,360
+analyse = False
+methodeLente = False
+var = 5
+alpha = 1.8
+gama = 6
+coeursLibres = 1
+modeliserImagesMultithread( pathCapteur, pathBdd, pathSave,nombreImages, nbPoints, resolutionOriginal, resolutionCapteur, largeur, longueur, minX,maxX,minY,maxY,minAngle,maxAngle, analyse,methodeLente, var, alpha , gama  ,coeursLibres)
