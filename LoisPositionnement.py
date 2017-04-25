554b460f17d6529ec3e36fbe2df88f215fa2ab55
Leduc <polchop@live.fr>
2017-04-25 15:49:06 +0200

    Version Finale

---
+++ LoisPositionnement.py
@@ -0,0 +1,24 @@
+import numpy
+from scipy.stats import norm
+
+
+def unePosition(minLongeur, maxLongueur, minLargeur,maxLargeur,minAngle,maxAngle):
+    """
+    Donne une valeur à 95% dans l'intervalle donnée selon une loi normal, pour les trois paramètres, x, y ,theta
+    
+    :param minLongeur: Position en x minimal
+    :param maxLongueur: Position eb x maximal
+    :param minLargeur: position eb y maximal
+    :param maxLargeur: position eb y maximal
+    :param minAngle: angle minimal
+    :param maxAngle: angle maximal
+    :return: Trois valeurs tirées de l'intervalle de confiance à 95%
+    """
+    x = numpy.random.normal((maxLargeur-minLargeur)/2, (maxLargeur - minLargeur) / (2*2.56))
+    y = numpy.random.normal((maxLongueur-minLongeur)/2, (maxLongueur - minLongeur) / (2*2.56))
+    theta = numpy.random.normal((maxAngle-minAngle)/2, (maxAngle - minAngle) / (2*2.56))
+    return x,y,theta
+
+#Main test
+position = unePosition(0,100,0,100,0,360)
+print(position)
--- Positionnement.py
+++
@@ -1,6 +0,0 @@
-import numpy
-def uneProsition(longeur, largeur):
-    x =  numpy.random.normal(0,1)
-    y = numpy.random.normal(0,1)
-    theta = numpy.random.normal(0,1)
-    return x,y,theta
