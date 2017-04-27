a796d9c6f12193e1352fe2f6e90694e4bff1099f
noemie6 <nonoemie6@hotmail.fr>
2017-04-27 11:21:08 +0200

    Update hdf5.py

--- hdf5.py
+++ hdf5.py
@@ -2,39 +2,43 @@ import h5py
 from PIL.Image import *
 
 
-def sauv(image):
+def sauv(image,nom,pathSave):
     """
     Enregistre l'image passee en parametre sous format hdf5 à l'adresse donnee par l'utilisateur ou dans un fichier
     prédéfinie, pour une génération random d'images.
     :param image: image à sauvegarder
+    :param nom: nom de l'image à sauvegarder
+    :param pathSave: adresse où enregistrer l'image
     :return: rajoute l'image dans le fichier h5 à l'adresse donnée par l'utilisateur ou créé un nouveau fichier s'il n'existe pas
     """
 
-    im = open(image)
-    #Demande du nom du fichier
-    fic = input("Entrez le nom du dossier : ")
-
-    #Demande du nom de l'utilisateur
-    numn = input("Entrez le numéro de l'utilisateur : ")
-    nomu="utilisateur : "+numn
-
-    #Demande du numéro du doigt
-    numd = input("Entrez le numéro du doigt : ")
-    nomd="doigt : "+numd
-
     #Création du fichier empreintedigitale
-    empdig = h5py.File(fic + ".h5")            # par défaut c'est , 'a' -> permet lecture et écriture s'il existe ou le créé s'il n'existe pas
+    empdig = h5py.File(pathSave + "/digi.h5")            # par défaut c'est , 'a' -> permet lecture et écriture s'il existe ou le créé s'il n'existe pas
 
-    if(nomu+"/"+nomd+"/"+image in empdig):
-            print("Il existe déjà une image de ce nom dans le chemin suivant :"+fic+"/"+nomu+"/"+nomd)
-    else:
-        pixels = list(im.getdata())
-        empdig.create_dataset(nomu + "/" + nomd + "/" + image, data=pixels, dtype='i')
+
+    j = 0
+    a=False
+    while(pathSave+nom in empdig):
+        j=j+1
+
+        # print("Il existe déjà une image de ce nom dans le chemin suivant :"+fic+"/"+nomu+"/"+nomd)
+        i = len(nom) - 1
+        print(nom)
+        print(i)
+        while (nom[i] != '.'):
+            print(nom[i])
+            i = i - 1
+        if(a):
+            nom = nom[:(i-1-len(str(j)))] + '_'+str(j) + nom[i:]
+        else:
+            nom=nom[:i]+'_'+str(j) +nom[i:]
+        a=True
+
+    pixels = list(image.getdata())
+    empdig.create_dataset(pathSave+ nom, data=pixels, dtype='i')
+    empdig[pathSave+nom]
     # Fermeture du fichier
     empdig.close()
 
-
-#Test de la méthode
-sauv('101_1.tif')
 
 
